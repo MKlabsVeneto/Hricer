8a624e1df9de15a63af23a43e51b50da5577e321
MKlabsVeneto <mklabsveneto@gmail.com>
2025-09-08 21:48:33 +0200

    modified:   Hricer001.py

---
+++ Hricer001.py
@@ -0,0 +1,17 @@
+# IMPORTS
+import os
+
+# LOGO AND SIGN
+print('Hricer, a dev and community driven python program to automatically install ricing programs and desktop stuff.    Copyright (C) 2025 Kevin De Togni, MKlabs')
+print('distribuited on the GNU general public license 3.0v license')
+print('          __________     __________')
+print('         /         /    /         /')
+print('        /         /    /         /')
+print('       /         /    /         /')
+print('      /         /____/         / __________  _______  _________   _________    __________          by MKlabs developer team')
+print('     /                        / /         / /      / /        /  /   _____/   /         /          devs: MrMaxX')
+print('    /                        / /         / /      / /   _____/  /   /        /         /')
+print('   /          ____          / /    _____/ /      / /   /       /   /_____   /    _____/            official website:')
+print('  /          /   /         / / /  |      /      / /   /_____  /    _____/  / /  |                  officialmklabsveneto.netlify.app')
+print(' /          /   /         / / ||  |     /      / /         / /    /_____  / ||  |')
+print('/__________/   /_________/ /__||__|    /______/ /_________/ /__________/ /__||__|                  version: 0.0.1 ALPHA')
